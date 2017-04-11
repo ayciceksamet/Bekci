c2eb91e2b41ccc3733add06b53d79c864dd46a43
Samet <ayciceksamet@gmail.com>
2017-04-11 17:23:21 +0300

    opencv libraries should be standalone.

---
+++ app/src/main/java/com/example/aycicek/bekci/open_camera.java
@@ -0,0 +1,13 @@
+package com.example.aycicek.bekci;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class open_camera extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.open_camera);
+    }
+}
