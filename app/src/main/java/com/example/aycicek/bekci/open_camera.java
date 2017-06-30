bb501ea7f67b8cfcd6d54e5175024b136817d2b1
Samet <ayciceksamet@gmail.com>
2017-07-01 02:20:08 +0300

    - image processing methods are added

--- app/src/main/java/com/example/aycicek/bekci/open_camera.java
+++ app/src/main/java/com/example/aycicek/bekci/open_camera.java
@@ -6,6 +6,7 @@ import android.graphics.Camera;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -26,12 +27,15 @@ import org.opencv.core.Core;
 import org.opencv.core.CvException;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 public class open_camera extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
@@ -46,6 +50,8 @@ public class open_camera extends Activity implements CameraBridgeViewBase.CvCame
     private Mat current_frame;
     private Mat previous_frame;
     private Mat diff_frame;
+    private boolean motionDetected = false;
+    List<MatOfPoint> contours;
 
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -127,30 +133,87 @@ public class open_camera extends Activity implements CameraBridgeViewBase.CvCame
     public void onCameraViewStopped() {
     }
 
-    public Mat isMotionDetected(Mat proccessing_frame){
-        proccessing_frame.copyTo(current_frame);
+    public Mat isMotionAvailable(Mat proccessingFrame){
+        proccessingFrame.copyTo(current_frame);
 
         if(inputFrame_first_time){
-            proccessing_frame.copyTo(previous_frame);
+            proccessingFrame.copyTo(previous_frame);
             inputFrame_first_time = false;
             Log.i("FIRST FRAME GET","FRAME LOG");
         }
         Core.absdiff(current_frame,previous_frame,diff_frame);
-        proccessing_frame.copyTo(previous_frame);
+        proccessingFrame.copyTo(previous_frame);
 
         return diff_frame;
     }
 
-    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+    public boolean isMotionReal(Mat processingRealFrame, List<MatOfPoint> contours ){
 
+        for(int i=0; i< contours.size();i++){
+            if (Imgproc.contourArea(contours.get(i)) > 500 ){
+                org.opencv.core.Rect rect = Imgproc.boundingRect(contours.get(i));
+                if (rect.height > 28){
+                    motionDetected=true;
+                }
+            }
+        }
+        return motionDetected;
+    }
+
+    public void processAlarm(boolean motionDetected) {
+        if(motionDetected) {
+            playAlarm();
+
+
+        }
+        else{
+            stopAlarm();
+        }
+
+    }
+
+    private void stopAlarm() {
+        MediaPlayer mp = MediaPlayer.create(this,
+                R.raw.alarm4);
+        mp.stop();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+
+        });
+
+    }
+    private void playAlarm() {
+        MediaPlayer mp = MediaPlayer.create(this,
+                R.raw.alarm4);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.release();
+            }
+
+        });
+
+    }
+
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         Mat mRgba = inputFrame.rgba();
         Imgproc.pyrDown(mRgba, mRgba);
-        Imgproc.blur(mRgba, mRgba, new Size(20.0, 20.0));
-        Imgproc.cvtColor(mRgba, mRgba, Imgproc.COLOR_RGB2GRAY);
-        Imgproc.adaptiveThreshold(mRgba, mRgba, 255,
-               Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY_INV, 15, 4);
-        Imgproc.dilate(mRgba, mRgba, Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT, new Size(20, 20)));
-        mRgba = isMotionDetected(mRgba);
+        mRgba = isMotionAvailable(mRgba);
+        Mat element = Imgproc.getStructuringElement(0, new org.opencv.core.Size(2*2 + 1, 2*2+1),new org.opencv.core.Point(2,2));
+        Imgproc.cvtColor(mRgba, mRgba, Imgproc.COLOR_BGRA2GRAY);
+        Imgproc.dilate(mRgba, mRgba, element);
+        Imgproc.threshold(mRgba, mRgba, 50, 255,Imgproc.THRESH_BINARY);
+        contours = new ArrayList<MatOfPoint>();
+        Imgproc.findContours(mRgba, contours, new Mat(), Imgproc.RETR_LIST,Imgproc.CHAIN_APPROX_SIMPLE);
+        motionDetected = isMotionReal(mRgba,contours);
+        processAlarm(motionDetected);
+        motionDetected=false;
         Imgproc.resize(mRgba,mRgba,inputFrame.rgba().size());
         return mRgba;
     }
